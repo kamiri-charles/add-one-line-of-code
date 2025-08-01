4e4403ab08832d0353b926f9d8e95ceb74276ae1
Charles Kamiri <charleskamiri6@gmail.com>
2025-08-01 22:11:29 +0300

    feat: Add auto deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './pages/landing'
 import Play from './pages/play'
 
