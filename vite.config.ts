4e06e32289e6b4154b8a9e3ad54ff440af304b68
Charles Kamiri <charleskamiri6@gmail.com>
2025-08-01 21:28:43 +0300

    fix: Resolve gh-pages deployment bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/",
+	base: "/add-one-line-of-code/",
 	plugins: [react(), tailwindcss()],
 });
